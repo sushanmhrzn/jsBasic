5d8bf57d8d8468fd5b324f678c258c05845a93a0
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-03-09 15:59:07 +0545

    Mesage

--- Day2/message.js
+++ Day2/message.js
@@ -1,7 +1,5 @@
 const message = () => {
-    const name = "Jesse";
-    const age = 40;
-    return name + ' is ' + age + 'years old.';
+    return "This is the message from message.js"
     };
     
     export default message;
